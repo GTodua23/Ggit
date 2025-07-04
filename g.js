40c70ae968c2fea3d573f6e0c7852b6e6f55b9e3
GTodua23 <gtodua232@gmail.com>
2025-07-04 17:44:31 +0400

    davaleba 8

---
+++ g.js
@@ -0,0 +1,10 @@
+var a=1 
+
+
+let student = { name: "John", age: 20, grades:[1,2,3,4]  };
+const student2 = { name: student.name,age:student.age+5, grades: 20,sxvastudent:student };
+
+console.log(student.name); // John
+console.log(student.grades); // 20
+student2.address="123 Main St";
+console.log(student2)
---
+++ gt.js
@@ -0,0 +1,50 @@
+/* 1 */ 
+
+var i=87;
+var g=65;
+
+console.log(i+g);
+
+/* 2 */ 
+
+var student3={firstname: "gio",lastname: "todua",age: 20,email: "g.t@gmsil.com",marks: 40};
+
+console.log(student3.firstname);
+
+/* 3 */ 
+
+
+var m=45;
+var n=23;
+
+if (m>n){console.log(m+n)} else {console.log(n)};
+
+/* 4 */ 
+
+console.log("Rise and shine," + student3.firstname +"!");
+
+/* 5 */ 
+
+var salary=1500;
+var bonus;
+
+if (salary>=2000){console.log(bonus=0)}
+else if (salary<2000){console.log(bonus=salary*0.1)};   /* if -ის გარეშე რო ვწერ ერორდება */ 
+
+/* 6 */ 
+
+var a=23;
+var b=a/2;
+var c=a+b;
+
+console.log(c)
+
+/* 7 */ 
+
+var phones=["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
+
+console.log(phones);
+/* 8 */ 
+
+console.log(phones[0]);
+console.log(phones[4]);
